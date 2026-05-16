4c7cd12ab5a8be14a141aaecc163c0e2c4287354
Byte Colony <goenka.ekansh@gmail.com>
2026-05-16 18:29:47 +0800

    tasks: reduce preemption timeslice from 10k to 1k instructions

--- public/script.js
+++ public/script.js
@@ -1977,7 +1977,7 @@ function toggleTheme() {
       '        if coroutine.status(t.co)=="dead" then',
       '          table.remove(_tasks,i)',
       '        elseif now>=t.wake then',
-      '          debug.sethook(t.co,function() coroutine.yield(_PREEMPT) end,"",10000)',
+      '          debug.sethook(t.co,function() coroutine.yield(_PREEMPT) end,"",1000)',
       '          local ok,rv=coroutine.resume(t.co)',
       '          debug.sethook(t.co)',
       '          if not ok then',
